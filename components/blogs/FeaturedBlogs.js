b86391c4a14f60ca62739e578ecba1afe870d8ad
bryanhuffstutler <bhuff8404@gmail.com>
2022-07-17 23:15:12 -0500

    add divider and featured blogs title

--- components/blogs/FeaturedBlogs.js
+++ components/blogs/FeaturedBlogs.js
@@ -1,17 +1,22 @@
-import { Grid } from "@material-ui/core";
+import { Divider, Grid, Typography } from "@material-ui/core";
 import BlogCard from "./BlogCard";
 
 function FeaturedBlogs(props) {
-    console.log(props.blogs)
-    return (
-        <Grid container spacing={4}>
-            {props.blogs.map(blog => {
-                return <BlogCard key={blog._id} id={blog._id} blog={blog} />
-            }
-            )}
-            
-        </Grid>
-    );
+  console.log(props.blogs);
+  return (
+    <div style={{margin: '20px'}}>
+    <Divider variant='fullWidth' orientation='horizontal' component='h1'/>
+      <Typography variant="h2" align="center" color="textPrimary">
+        Featured Blogs
+      </Typography>
+      <Divider variant='fullWidth' orientation='horizontal' component='h1'/>
+      <Grid container spacing={4} style={{margin: '20px'}}>
+        {props.blogs.map((blog) => {
+          return <BlogCard key={blog._id} id={blog._id} blog={blog} />;
+        })}
+      </Grid>
+    </div>
+  );
 }
 
 export default FeaturedBlogs;
