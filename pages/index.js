b052b11ea69a9b70b050388a79af3b4adb784474
bryanhuffstutler <bhuff8404@gmail.com>
2022-07-17 23:24:29 -0500

    fixed canonical

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,38 @@ import Head from "next/head";
 import FeaturedBlogs from "../components/blogs/FeaturedBlogs";
 import Intro from "../components/Intro/Intro";
 import dbConnect from "../utils/dbConnect";
-import Blog from '../models/Blog'
+import Blog from "../models/Blog";
 
 export default function Home(props) {
-  const blogs = JSON.parse(props.blogs)
+  const blogs = JSON.parse(props.blogs);
 
   return (
     <div>
       <Head>
         <title>@Bryan_FS_Dev's Blog</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="@Bryan_FS_DEV's blog. I blog about life in general, I write technical blogs, and I write about being a developer in general." />
+        <link rel="canonical" href="https://bryan-fs-dev-blog.vercel.app/" />
+        <meta
+          name="description"
+          content="@Bryan_FS_DEV's blog. I blog about life in general, I write technical blogs, and I write about being a developer in general."
+        />
       </Head>
 
       <Intro />
-      
+
       {blogs ? <FeaturedBlogs blogs={blogs} /> : "No Featured Blogs"}
-      
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   dbConnect();
-  const data = await Blog.find({ isFeatured: true })
-  const blogs = JSON.stringify(data)
+  const data = await Blog.find({ isFeatured: true });
+  const blogs = JSON.stringify(data);
 
-  
   return {
     props: {
-      blogs: blogs
+      blogs: blogs,
     },
   };
 }
